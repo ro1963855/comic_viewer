54bfc2f23db2ae192a9d11b30096895fb81fdf97
Dandy <dandy.chen@advantech.com.tw>
2018-07-06 14:54:57 +0800

    finish navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="cv_navbar">
+    <header class="cv-nabar-header d-flex justify-content-center align-items-center">
+      <span class="title">Comicomic</span>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'cv_navbar',
+};
+</script>
