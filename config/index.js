13e161c6a307d870cfd55f8598f3f66b66c273e2
Dandy.Chen <Dandy.Chen@advantech.com.tw>
2019-04-18 11:32:25 +0800

    for Heroku deploy

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: 'comic-viewer-dandy.herokuapp.com/',
+    assetsPublicPath: '/',
 
     /**
      * Source Maps
